292fa901e7e4c4e2e8f78a07973aa2d3f930d1e7
kminoda <koji.minoda@tier4.jp>
2022-11-21 16:04:05 +0900

    michinakaba
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- include/pclomp/voxel_grids_manager.h
+++ include/pclomp/voxel_grids_manager.h
@@ -11,30 +11,63 @@
 
 namespace pclomp
 {
-  template<typename PointTarget>
-  class VoxelGridsManager
-  {
-    protected:
-        /** \brief Typename of searchable voxel grid containing mean and covariance. */
-		typedef pclomp::VoxelGridCovariance<PointTarget> TargetGrid;
-		/** \brief Typename of pointer to searchable voxel grid. */
-		typedef TargetGrid* TargetGridPtr;
-		/** \brief Typename of const pointer to searchable voxel grid. */
-		typedef const TargetGrid* TargetGridConstPtr;
-		/** \brief Typename of const pointer to searchable voxel grid leaf. */
-		typedef typename TargetGrid::LeafConstPtr TargetGridLeafConstPtr;
-        
-        using PointCloudTarget = pcl::PointCloud<PointTarget>;
-        using PointCloudTargetConstPtr = typename PointCloudTarget::ConstPtr;
 
-        std::map<std::string, TargetGrid> target_cells_dict_;
+template<typename PointTarget> class VoxelGridsManager
+{
+protected:
+  /** \brief Typename of searchable voxel grid containing mean and covariance. */
+  typedef pclomp::VoxelGridCovariance<PointTarget> TargetGrid;
+  /** \brief Typename of pointer to searchable voxel grid. */
+  typedef TargetGrid* TargetGridPtr;
+  /** \brief Typename of const pointer to searchable voxel grid. */
+  typedef const TargetGrid* TargetGridConstPtr;
+  /** \brief Typename of const pointer to searchable voxel grid leaf. */
+  typedef typename TargetGrid::LeafConstPtr TargetGridLeafConstPtr;
 
-    public:
-        VoxelGridsManager();
-        void removeVoxelGridMap(const std::string mapID);
-        void addVoxelGridMap(const std::string mapID, const PointCloudTargetConstPtr target);
-        void getNeighborhood(const PointTarget& reference_point, std::vector<TargetGridLeafConstPtr> &neighbors) const ;
+  using PointCloudTarget = pcl::PointCloud<PointTarget>;
+  using PointCloudTargetConstPtr = typename PointCloudTarget::ConstPtr;
+
+  struct LeafID {
+    std::string voxel_id;
+    int leaf_id;
+    bool operator < (const LeafID& rhs) const {
+      if (voxel_id < rhs.voxel_id) {
+        return true;
+      }
+      if (voxel_id > rhs.voxel_id) {
+        return false;
+      }
+      if (leaf_id < rhs.leaf_id) {
+        return true;
+      }
+      if (leaf_id > rhs.leaf_id) {
+        return false;
+      }
+      return false;
+    }
   };
-}
+
+public:
+  VoxelGridsManager();
+  void removeVoxelGridMap(const std::string map_id);
+  void addVoxelGridMap(const std::string map_id, const PointCloudTargetConstPtr target);
+  void getNeighborhood(const PointTarget& reference_point, std::vector<TargetGridLeafConstPtr> &neighbors) const;
+  int radiusSearch (
+    const PointT & point, double radius, std::vector<LeafConstPtr> & k_leaves,
+    std::vector<float> & k_sqr_distances, unsigned int max_nn = 0) const;
+  int getNeighborhoodAtPoint(const PointT & reference_point, std::vector<LeafConstPtr> & neighbors) const;
+  int getNeighborhoodAtPoint1(const PointT & reference_point, std::vector<LeafConstPtr> & neighbors) const;
+  int getNeighborhoodAtPoint7(const PointT & reference_point, std::vector<LeafConstPtr> & neighbors) const;
+
+private:
+	std::map<LeafID, Leaf> leaves_;
+  PointCloudPtr voxel_centroids_;
+  std::vector<LeafID> voxel_centroids_leaf_indices_;
+
+  pcl::KdTreeFLANN<PointT> kdtree_;
+  std::map<std::string, TargetGrid> target_cells_dict_;
+
+};
+} // namespace pclomp
 
 #endif // PCLOMP_VOXEL_GRIDS_MANAGER_H_
--- include/pclomp/voxel_grids_manager_impl.hpp
+++ include/pclomp/voxel_grids_manager_impl.hpp
@@ -4,24 +4,98 @@
 #define PCLOMP_VOXEL_GRIDS_MANAGER_IMPL_HPP_
 
 
-// template<typename PointTarget> void
-// pclomp::VoxelGridsManager<PointTarget>::VoxelGridsManager () {}
-
-template<typename PointTarget> void
-pclomp::VoxelGridsManager<PointTarget>::addVoxelGridMap(
-  const std::string mapID, const PointCloudTargetConstPtr target)
+template<typename PointTarget>
+void pclomp::VoxelGridsManager<PointTarget>::VoxelGridsManager()
+: target_cells_dict_()
 {
-  (void)mapID;
-  (void)target;
 }
 
-template<typename PointTarget> void
-pclomp::VoxelGridsManager<PointTarget>::getNeighborhood(
-  const PointTarget& reference_point,
-  std::vector<TargetGridLeafConstPtr> &neighbors) const
+template<typename PointTarget>
+void pclomp::VoxelGridsManager<PointTarget>::addVoxelGridMap(
+  const std::string map_id, const PointCloudTargetConstPtr target)
+{
+  TargetGrid target_grid = nullptr;
+  target_cells_dict_[map_id] = target_grid;
+}
+
+template<typename PointTarget>
+void pclomp::VoxelGridsManager<PointTarget>::removeVoxelGridMap(
+  const std::string map_id)
+{
+  target_cells_dict_.erase(map_id);
+}
+
+template<typename PointTarget>
+void pclomp::VoxelGridsManager<PointTarget>::createKdTree()
+{
+  if (!searchable_ || (voxel_centroids_->size() == 0)) return;
+  kdtree_.setInputCloud (voxel_centroids_);
+  return;
+}
+
+template<typename PointTarget>
+int pclomp::VoxelGridsManager<PointTarget>::radiusSearch (
+  const PointT & point, double radius, std::vector<LeafConstPtr> & k_leaves,
+  std::vector<float> & k_sqr_distances, unsigned int max_nn = 0) const
+{
+  k_leaves.clear ();
+
+  // // Check if kdtree has been built
+  // if (!searchable_)
+  // {
+  //   PCL_WARN ("%s: Not Searchable", this->getClassName().c_str ());
+  //   return 0;
+  // }
+
+  // Find neighbors within radius in the occupied voxel centroid cloud
+  std::vector<int> k_indices;
+  int k = kdtree_.radiusSearch (point, radius, k_indices, k_sqr_distances, max_nn);
+
+  // Find leaves corresponding to neighbors
+  k_leaves.reserve (k);
+  for (std::vector<int>::iterator iter = k_indices.begin (); iter != k_indices.end (); iter++)
+  {
+    auto leaf = leaves_.find(voxel_centroids_leaf_indices_[*iter]);
+    if (leaf == leaves_.end()) {
+      std::cerr << "error : could not find the leaf corresponding to the voxel" << std::endl;
+      std::cin.ignore(1);
+    }
+    k_leaves.push_back (&(leaf->second));
+  }
+  return k;
+}
+
+template<typename PointTarget>
+int getNeighborhoodAtPoint(const PointT & reference_point, std::vector<LeafConstPtr> & neighbors) const
 {
   (void)reference_point;
   (void)neighbors;
+  throw std::runtime_error;
+}
+
+template<typename PointTarget>
+int getNeighborhoodAtPoint(const PointT & reference_point, std::vector<LeafConstPtr> & neighbors) const
+{
+  (void)reference_point;
+  (void)neighbors;
+  throw std::runtime_error;
+}
+
+
+template<typename PointTarget>
+int getNeighborhoodAtPoint1(const PointT & reference_point, std::vector<LeafConstPtr> & neighbors) const
+{
+  (void)reference_point;
+  (void)neighbors;
+  throw std::runtime_error;
+}
+
+template<typename PointTarget>
+int getNeighborhoodAtPoint7(const PointT & reference_point, std::vector<LeafConstPtr> & neighbors) const
+{
+  (void)reference_point;
+  (void)neighbors;
+  throw std::runtime_error;
 }
 
 #endif // PCLOMP_VOXEL_GRIDS_MANAGER_IMPL_HPP_
